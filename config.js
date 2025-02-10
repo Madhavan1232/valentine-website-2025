1c042f17153166d7b70588cb3674d26b34f08ed7
Madhavan1232 <152589549+Madhavan1232@users.noreply.github.com>
2025-02-10 19:44:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://mobcup.com.co/download/die-with-a-smile-song-ringtone-download-pobtyx3/mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dhujdvvyi/video/upload/v1739196668/cfsbpjd6k1oyi5kmqieq.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 1.0                        // Volume level (0.0 to 1.0)
